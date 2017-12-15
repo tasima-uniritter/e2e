aaa426034f9b0318009ace6da61a4458581b3a20
Hyan Mandian <hyanmandian@hotmail.com>
2017-12-15 21:55:36 -0200

    [fix] Adjust chebkox

--- features/step_definitions/newsletter.rb
+++ features/step_definitions/newsletter.rb
@@ -7,7 +7,7 @@ When /^I click on submit in newsletter form$/ do
 end
 
 When /^I choose one favorite content$/ do
-    check('.newsletter-list-label input')
+    find('.newsletter-list-label input').set(true)
 end
 
 When /^I sign me up on newsletter with invalid email$/ do
