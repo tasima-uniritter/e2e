18380d78917bf624cfbddddf1fa101ce037547fc
Hyan Mandian <hyanmandian@hotmail.com>
2018-01-04 10:07:22 -0200

    [fix] Adjust newsletter test

--- features/step_definitions/newsletter.rb
+++ features/step_definitions/newsletter.rb
@@ -7,7 +7,7 @@ When /^I click on submit in newsletter form$/ do
 end
 
 When /^I choose one favorite content$/ do
-    find('.newsletter-checkbox', :visible => true).first().set(true)
+    find('.newsletter-checkbox', :visible => false).set(true)
 end
 
 When /^I sign me up on newsletter with invalid email$/ do
