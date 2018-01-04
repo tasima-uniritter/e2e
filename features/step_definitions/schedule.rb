61a7ac64ef0684b236ae52ca31fe9a428512c63e
Hyan Mandian <hyanmandian@hotmail.com>
2018-01-04 10:56:23 -0200

    [feat] Schedule tests

--- features/step_definitions/schedule.rb
+++ features/step_definitions/schedule.rb
@@ -2,12 +2,12 @@ When /^I click on schedule$/ do
     find("[data-for='schedule']").click();
 end
 
-When /^I click on "(.*)" in shedule nav$/ do |weekday|
+When /^I click on (.*) in shedule nav$/ do |weekday|
     within('.days-list') do
         click(weekday);
     end
 end
 
-Then /^I can see "(.*)" tb programs$/ do |length|
+Then /^I can see (.*) tv programs$/ do |length|
     expect(page).to have_selector('.event', count: length)
 end
