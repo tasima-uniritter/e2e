a7ed6428989eabd3c49d4ad5b6424c4424750cec
Hyan Mandian <hyanmandian@hotmail.com>
2018-01-04 11:39:39 -0200

    [fix] Adjust all tests

--- features/newsletter.feature
+++ features/newsletter.feature
@@ -1,6 +1,6 @@
-# Feature: Newsletter
-#     Scenario: Sign up with invalid email
-#         Given I access Gaucha
-#         When I sign me up on newsletter with invalid email
-#         And I click on submit in newsletter form
-#         Then I can see error message inside newsletter box
+Feature: Newsletter
+    Scenario: Sign up with invalid email
+        Given I access Gaucha
+        When I sign me up on newsletter with invalid email
+        And I click on submit in newsletter form
+        Then I can see error message inside newsletter box
--- features/support/env.rb
+++ features/support/env.rb
@@ -32,18 +32,4 @@ end
 
 Capybara.javascript_driver = :headless_chrome
 
-module Helpers
-  def element_visible?(element_id)
-      exists = page.has_css?(element_id)
-      self_hidden = page.has_css?("#{element_id}.ui-helper-hidden")
-      parent_hidden = page.has_css?(".ui-helper-hidden > #{element_id}")
-      other_ancestor_hidden = page.has_css?(".ui-helper-hidden * #{element_id}")
-      invisible = self_hidden || parent_hidden || other_ancestor_hidden
-  
-      return (exists && !invisible)
-  end
-end
-
-World(Helpers)
-
-World(Capybara::DSL, Helpers)
+World(Capybara::DSL)
