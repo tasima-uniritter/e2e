785a34389960cd14765af2f4351a2f5641a4fca4
Hyan Mandian <hyanmandian@hotmail.com>
2018-01-04 11:10:37 -0200

    [feat] Schedule tests

--- features/schedule.feature
+++ features/schedule.feature
@@ -1,5 +1,6 @@
 Feature: Schedule
     Scenario: See monday schedule
-        Given I access Gaucha schedule
+        Given I access Gaucha
+        When I click on schedule
         And I click on SEG in shedule nav
         Then I can see 17 tv programs
--- features/step_definitions/schedule.rb
+++ features/step_definitions/schedule.rb
@@ -1,9 +1,13 @@
-Given /^I access Gaucha schedule$/ do
-    visit('https://gauchazh.clicrbs.com.br/programacao/')
+weekdays = {
+    "SEG" => "/html/body/div[1]/div/div[4]/div/div/div/div/section[1]/div/nav[1]/ul/li[5]/button",
+}
+
+When /^I click on schedule$/ do
+    find("[data-for='schedule']").click();
 end
 
 When /^I click on (.*) in shedule nav$/ do |weekday|
-    click_button(weekday);
+    find(weekdays[weekday]).click();
 end
 
 Then /^I can see (.*) tv programs$/ do |length|
