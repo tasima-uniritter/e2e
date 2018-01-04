34206e5665719300042340c6d9381ed9900d6b1e
Hyan Mandian <hyanmandian@hotmail.com>
2018-01-04 10:26:19 -0200

    [fix] Adjust newsletter test

--- features/step_definitions/newsletter.rb
+++ features/step_definitions/newsletter.rb
@@ -7,7 +7,7 @@ When /^I click on submit in newsletter form$/ do
 end
 
 When /^I choose one favorite content$/ do
-    find('[for=mce-group[5]-5-0]').click()
+    find('.newsletter-list-label').click()
 end
 
 When /^I sign me up on newsletter with invalid email$/ do
